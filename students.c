b725b8cc560cd06fa7e339ca564ee684d0cc77e7
friesemi <35504972+friesemi@users.noreply.github.com>
2018-10-10 14:06:43 -0700

    student array test

--- students.c
+++ students.c
@@ -85,8 +85,9 @@ struct dynarray* create_student_array(int num_students, char** names, int* ids,
     float* gpas) {
 	int i = 0;
 	struct dynarray *arr = dynarray_create();
-	for (i; i < num_students; i++) {
-		dynarray_insert(arr, -1, create_student(names[i], ids[i], gpas[i]));
+	for (i; i < num_students; i++)
+		struct student *stud = create_student(names[i], ids[i], gpas[i]);
+		dynarray_insert(arr, -1, stud);
 	}
 	return arr;
 }
