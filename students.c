ec247857ae7c12a5577e3a1009cb76002730b947
friesemi <35504972+friesemi@users.noreply.github.com>
2018-10-09 19:03:04 -0700

    create population correctly

--- students.c
+++ students.c
@@ -86,7 +86,7 @@ struct dynarray* create_student_array(int num_students, char** names, int* ids,
 	int i = 0;
 	struct dynarray *arr = dynarray_create();
 	for (i; i < num_students; i++) {
-		dynarray_insert(arr, -1, create_student(names[i], ids[i], gpas[i]));
+		dynarray_insert(arr, i, create_student(names[i], ids[i], gpas[i]));
 	}
 	return arr;
 }
