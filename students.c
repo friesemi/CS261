fde0541a6151dda426996eb49ef9d222aff70cf8
friesemi <35504972+friesemi@users.noreply.github.com>
2018-10-11 18:09:10 -0700

    student array fix

--- students.c
+++ students.c
@@ -91,9 +91,7 @@ struct dynarray* create_student_array(int num_students, char** names, int* ids,
 	int i = 0;
 	struct dynarray *arr = dynarray_create();
 	for (i; i < num_students; i++){
-		struct student *stud = create_student(names[i], ids[i], gpas[i]);//the memory isn't allocated correctly
-		assert(stud);
-		dynarray_insert(arr, -1, stud);
+		dynarray_insert(arr, -1, create_student(names, ids, gpas));
 	}
 	return arr;
 }
