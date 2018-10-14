146c612217133cfceefee626c22b176a65f167dc
friesemi <35504972+friesemi@users.noreply.github.com>
2018-10-14 16:56:21 -0700

    sort fixes

--- students.c
+++ students.c
@@ -230,9 +230,8 @@ void sort_by_gpa(struct dynarray* students) {
 			second = dynarray_get(students, j + 1);
 			if (second->gpa > first->gpa) {//if second val is greater than first val
 				printf("is larger than\n");
-				temp = first;
-				second = first;
-				temp = temp;
+				dynarray_set(students, j+1, first);
+				dynarray_set(students, j, second);
 			}
 		}
 	}
